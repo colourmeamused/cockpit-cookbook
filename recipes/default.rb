9e744088e4c2d88109621ce69c5aff5e65f3560c
colourmeamused <colourmeamused@noreply.com>
2016-07-18 01:54:27 +1000

    fix syntax

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,13 +1,11 @@
-
-
 case node['platform']
 when 'rhel'
   rhsm_repo 'rhel-7-server-extras-rpms' do
     action :enable
   end
-
   package 'cockpit'
   firewalld_service 'cockpit'
   service 'cockpit' do
     action [:enable, :start]
   end
+end
